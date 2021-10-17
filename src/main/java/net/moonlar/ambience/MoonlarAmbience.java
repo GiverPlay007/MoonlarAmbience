3e01bf7c13ff66af0e56f2160d3f41483378ca71
GiverPlay007 <play.giver.007@gmail.com>
2021-10-17 13:30:01 -0300

    Initialize project

---
+++ src/main/java/net/moonlar/ambience/MoonlarAmbience.java
@@ -0,0 +1,16 @@
+package net.moonlar.ambience;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MoonlarAmbience extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+
+  }
+
+  @Override
+  public void onDisable() {
+
+  }
+}
