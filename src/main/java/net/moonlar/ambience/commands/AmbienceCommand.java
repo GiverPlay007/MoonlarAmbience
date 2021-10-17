1110cf7edf2ee40302dcbd70ebda453d3e168f35
GiverPlay007 <play.giver.007@gmail.com>
2021-10-17 13:56:14 -0300

    Make plugin reloadable

--- src/main/java/net/moonlar/ambience/commands/AmbienceCommand.java
+++ src/main/java/net/moonlar/ambience/commands/AmbienceCommand.java
@@ -1,6 +1,7 @@
 package net.moonlar.ambience.commands;
 
 import net.moonlar.ambience.MoonlarAmbience;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -16,13 +17,18 @@ public class AmbienceCommand implements CommandExecutor {
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-    if(args.length == 0) {
+    if(args.length == 0 || !sender.hasPermission("moonlar.ambience.admin")) {
       plugin.sendVersion(sender);
       return true;
     }
 
+    if(args[0].equalsIgnoreCase("reload")) {
+      plugin.reload();
+      sender.sendMessage(ChatColor.GREEN + "Configurações recarregadas.");
+      return true;
+    }
+
+    sender.sendMessage(ChatColor.RED + "Sub-comando inválido!");
     return true;
   }
-
-
 }
